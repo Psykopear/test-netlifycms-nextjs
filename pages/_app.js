e4f17dda1338fdd3030031d698b0959b13116c7f
Federico Dolce <psykopear@gmail.com>
2021-01-26 22:16:00 +0100

    Added webgl fluid simulation

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ const App = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Michele Dolce - Research Assistant in Mathematics at Imperial College London" />
         <title>Michele Dolce</title>
--- pages/research.js
+++ pages/research.js
@@ -1,9 +1,22 @@
+import React, { useState } from "react";
 import Section from "../components/section.js";
+import FluidAnimation from "react-fluid-animation";
 import Link from "next/link";
 import { attributes, react as Content } from "../content/research.md";
 
+const animationConfig = {
+  textureDownsample: 1,
+  densityDissipation: 0.98,
+  velocityDissipation: 0.99,
+  pressureDissipation: 0.8,
+  pressureIterations: 25,
+  curl: 30,
+  splatRadius: 0.005,
+};
+
 const Research = () => {
   const { papers } = attributes;
+  const [simulation, setSimulation] = useState(false);
   return (
     <div className="wrapper">
       <Section>
@@ -14,17 +27,30 @@ const Research = () => {
               <li key={p.title}>
                 {p.author} <i>{`"${p.title}"`}</i> {p.extra}{" "}
                 {p.links.map((l) => (
-                  <>
-                    <Link key={l.url} href={l.url}>
-                      {l.name}
-                    </Link>{" "}
-                  </>
+                  <React.Fragment key={l.url}>
+                    <Link href={l.url}>{l.name}</Link>{" "}
+                  </React.Fragment>
                 ))}{" "}
                 ({p.year})
               </li>
             ))}
           </ol>
         </>
+      </Section>
+      <Section dark>
+        {simulation ? (
+          <>
+            <h3>Click and drag the mouse around</h3>
+            <FluidAnimation
+              config={animationConfig}
+              style={{ height: "500px" }}
+            />
+          </>
+        ) : (
+          <div onClick={() => setSimulation(true)} style={{ height: "500px" }}>
+            <h3>Click for a fluid simulation</h3>
+          </div>
+        )}
       </Section>
     </div>
   );
